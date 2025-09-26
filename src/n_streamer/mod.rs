839ef56f536a5dbe4f0c6dbd20b2657efbacf5e1
Felix Drodofsky <drodofsky@protonmail.com>
2025-09-26 20:14:34 +0200

    prototype gui

--- src/n_streamer/mod.rs
+++ src/n_streamer/mod.rs
@@ -1,6 +1,11 @@
 mod message;
 
-use iced::{Element, Task, widget::text};
+use iced::{
+    Element,
+    Length::Fill,
+    Task,
+    widget::{Space, column, container, row, text},
+};
 
 use message::Message;
 
@@ -18,6 +23,19 @@ impl NStreamer {
         Task::none()
     }
     pub fn view(&self) -> Element<'_, Message> {
-        text("Hello World!").into()
+        column![self.view_top(), text("Hello World!"),].into()
+    }
+    fn view_menu(&self) -> Element<'_, Message> {
+        text("menu").into()
+    }
+    fn view_top(&self) -> Element<'_, Message> {
+        container(row![
+            self.view_menu(),
+            Space::with_width(Fill),
+            text("19::32")
+        ])
+        .padding(6)
+        .style(container::bordered_box)
+        .into()
     }
 }
