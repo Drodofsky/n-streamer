58b120011db36b3ea6f14c3fe5f69d652d88b245
Felix Drodofsky <drodofsky@protonmail.com>
2025-09-29 13:05:00 +0200

    fix warnings

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use crate::n_streamer::NStreamer;
 mod n_streamer;
 
 fn main() -> iced::Result {
-    iced::application(|| NStreamer::init(), NStreamer::update, NStreamer::view)
+    iced::application(NStreamer::init, NStreamer::update, NStreamer::view)
         .subscription(NStreamer::subscription)
         .exit_on_close_request(false)
         .run()
