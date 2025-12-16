093e38e02bd8475f2142bf7c78e01db5a5034441
Felix Drodofsky <drodofsky@protonmail.com>
2025-12-16 15:19:25 +0100

    remove CHECK constraints as it is not yet supported in turso

--- src/db/create_table_episode.sql
+++ src/db/create_table_episode.sql
@@ -4,14 +4,14 @@ CREATE TABLE IF NOT EXISTS episode (
     episode_id      INTEGER NOT NULL,
     episode_title   TEXT NULL,  -- Option<String>
 
-    suspend_flg     INTEGER NOT NULL CHECK (suspend_flg IN (0,1)),
+    suspend_flg     INTEGER NOT NULL,
 
     schedule        DATETIME NOT NULL,
     period          INTEGER NOT NULL, -- store seconds
 
-    rebroadcast_flg INTEGER NULL CHECK (rebroadcast_flg IN (0,1)), -- Option<bool>
-    bilingual_flg   INTEGER NULL CHECK (bilingual_flg IN (0,1)),   -- Option<bool>
-    english_flg     INTEGER NULL CHECK (english_flg IN (0,1)),     -- Option<bool>
+    rebroadcast_flg INTEGER NULL,
+    bilingual_flg   INTEGER NULL, 
+    english_flg     INTEGER NULL, 
 
     PRIMARY KEY (program_id, episode_id, schedule)
 );
