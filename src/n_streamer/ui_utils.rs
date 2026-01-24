242621316fd5b4daf2558cf798387e11dad49250
Felix Drodofsky <drodofsky@protonmail.com>
2026-01-24 20:29:59 +0100

    impl view_top

--- src/n_streamer/ui_utils.rs
+++ src/n_streamer/ui_utils.rs
@@ -1,6 +1,8 @@
 use super::*;
 use iced::{Renderer, Theme, widget::Container};
+pub const PADDING: u16 = 5;
 pub const BIG_PADDING: u16 = 10;
+pub const SPACING: u32 = 5;
 pub fn primary_text<'s>(text: String) -> Container<'s, Message, Theme, Renderer> {
     iced::widget::container(
         iced::widget::text(text).style(|theme| iced::widget::text::Style {
@@ -19,4 +21,7 @@ pub fn primary_text<'s>(text: String) -> Container<'s, Message, Theme, Renderer>
     })
     .padding(BIG_PADDING)
     .align_x(iced::Center)
+}
+pub fn text_button(text: &str) -> iced::widget::Button<'_, Message, Theme, Renderer> {
+    iced::widget::button(iced::widget::text(text).align_x(iced::Alignment::Center))
 }
--- src/n_streamer/view.rs
+++ src/n_streamer/view.rs
@@ -1,8 +1,29 @@
-use iced::Element;
+use iced::{
+    Alignment, Element,
+    widget::{container, row},
+};
 
 use super::*;
 impl NStreamer {
     pub fn view(&self) -> Element<'_, Message> {
-        self.clock.view()
+        self.view_top()
+    }
+    fn view_top(&self) -> Element<'_, Message> {
+        container(
+            row![
+                text_button("Settings"),
+                text_button("Program Schedule"),
+                text_button("Watch Live"),
+                text_button("current program"),
+                text_button("Manage Downloads"),
+                text_button("Library"),
+                self.clock.view()
+            ]
+            .spacing(SPACING)
+            .align_y(Alignment::Center),
+        )
+        .padding(PADDING)
+        .style(container::bordered_box)
+        .into()
     }
 }
