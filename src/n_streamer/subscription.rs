c526d4b9966031688bf47aeceadc3ee41ddd2c63
Felix Drodofsky <drodofsky@protonmail.com>
2026-01-24 20:19:25 +0100

    impl clock

--- src/n_streamer/subscription.rs
+++ src/n_streamer/subscription.rs
@@ -1,8 +1,11 @@
+use std::time::Duration;
+
 use iced::Subscription;
 
 use super::*;
 impl NStreamer {
     pub fn subscription(&self) -> Subscription<Message> {
-        Subscription::batch([])
+        let tick = iced::time::every(Duration::from_millis(500)).map(|_| Message::Tick);
+        Subscription::batch([tick])
     }
 }
--- src/n_streamer/view.rs
+++ src/n_streamer/view.rs
@@ -3,6 +3,6 @@ use iced::Element;
 use super::*;
 impl NStreamer {
     pub fn view(&self) -> Element<'_, Message> {
-        iced::widget::text("Hello world").into()
+        self.clock.view()
     }
 }
