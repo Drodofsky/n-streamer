a33328907cd9d085f0d818be439e137bff0c9d47
Felix Drodofsky <drodofsky@protonmail.com>
2026-01-24 18:09:29 +0100

    clear up project to start over with more tests and a cleaner architecture

--- src/db/get_episode.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM episode 
-WHERE suspend_flg = 0
-AND schedule > ?1
-
-ORDER BY schedule ASC;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use crate::n_streamer::NStreamer;
-
-mod n_streamer;
-
 fn main() -> iced::Result {
-    iced::application(NStreamer::init, NStreamer::update, NStreamer::view)
-        .subscription(NStreamer::subscription)
-        .theme(NStreamer::theme)
-        .exit_on_close_request(false)
-        .run()
+    todo!()
 }
