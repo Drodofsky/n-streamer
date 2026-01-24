7590eead6c5273494828121b528bf99b4f9cf867
Felix Drodofsky <drodofsky@protonmail.com>
2026-01-24 21:36:08 +0100

    impl (exit) user interaction

--- src/n_streamer/subscription.rs
+++ src/n_streamer/subscription.rs
@@ -1,11 +1,13 @@
 use std::time::Duration;
 
-use iced::Subscription;
+use iced::{Subscription, window};
 
 use super::*;
 impl NStreamer {
     pub fn subscription(&self) -> Subscription<Message> {
         let tick = iced::time::every(Duration::from_millis(500)).map(|_| Message::Tick);
-        Subscription::batch([tick])
+        let close =
+            window::close_requests().map(|id| Message::Window(WindowMessage::ExitRequest(id)));
+        Subscription::batch([tick, close])
     }
 }
