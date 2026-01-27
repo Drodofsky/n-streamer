399f2efdf208ce6b53f30ccebe36d367cc2f3464
Felix Drodofsky <drodofsky@protonmail.com>
2026-01-27 15:55:42 +0100

    impl tests for theming

---
+++ tests/settings/theme.rs
@@ -0,0 +1,40 @@
+use super::*;
+use iced_test::simulator;
+use n_streamer::*;
+#[tokio::test]
+async fn select_white_theme() {
+    let mut n_streamer = NStreamer::default();
+
+    let task = n_streamer.update(Message::Settings(SettingsMessage::SettingSelected(
+        SettingItem::Theme,
+    )));
+    execute_tasks(task, &mut n_streamer).await;
+    let mut ui = simulator(n_streamer.view());
+
+    let _ = ui.click("Light").unwrap();
+    for message in ui.into_messages() {
+        let task = n_streamer.update(message);
+        execute_tasks(task, &mut n_streamer).await;
+    }
+
+    assert_eq!(n_streamer.theme(), iced::Theme::Light);
+}
+
+#[tokio::test]
+async fn select_dark_theme() {
+    let mut n_streamer = NStreamer::default();
+
+    let task = n_streamer.update(Message::Settings(SettingsMessage::SettingSelected(
+        SettingItem::Theme,
+    )));
+    execute_tasks(task, &mut n_streamer).await;
+    let mut ui = simulator(n_streamer.view());
+
+    let _ = ui.click("Dark").unwrap();
+    for message in ui.into_messages() {
+        let task = n_streamer.update(message);
+        execute_tasks(task, &mut n_streamer).await;
+    }
+
+    assert_eq!(n_streamer.theme(), iced::Theme::Dark);
+}
